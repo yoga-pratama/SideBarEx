1396ed82172d4a46c67cf75347bbbf1b773cbb86
yoga <yogamadness@gmail.com>
2018-11-12 13:45:42 +0700

    Initial Commit

---
+++ SideBarEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideBarEx
+//
+//  Created by Yoga Pratama on 12/11/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
